8a0b2d0bea576d92207ec3af5d9234610abce302
Elvis Li <presley.us@gmail.com>
2021-10-27 20:02:15 -0400

    configured the local mongodb on the web app

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,6 +1,6 @@
 
 
-export const LocalURI = "mongodb://localhost/books229";
+export const LocalURI = "mongodb://localhost/store";
 export const RemoteURI = "your remote host information goes here"
 export const HostName = "LocalHost";
 export const Secret = "someSecret";
